6ea20dc8dd57ddd498a7dc1d08f1c022dd8cb1ac
Hanna Pon <hannainleto@mail.ru>
2022-08-25 17:26:02 +0300

    first commit task

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a==b*b)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
